5067c47f59a8be1794db35ffe925f79cc55f22c8
Rayhan Elbeera <raybeera2014@gmail.com>
2024-03-12 09:32:59 +0000

    getAllArticles component rendring

--- nc_news/src/assets/Components/ArticlesList.jsx
+++ nc_news/src/assets/Components/ArticlesList.jsx
@@ -1,25 +1,41 @@
 import { useState, useEffect } from "react";
 import { getArticles } from "../../../api";
+import { Link } from "react-router-dom";
 
 const ArticlesList = () => {
-  const[ allArticles, setAllArticles] = useState([])
+  const [allArticles, setAllArticles] = useState([]);
 
   useEffect(() => {
     getArticles().then((response) => {
       setAllArticles(response.data.articles);
     });
   }, []);
+
   return (
-    <ul>
+    <ul className="articles-list">
       {allArticles.map((article) => {
-        console.log(article)
+        console.log(article);
         return (
-         <li>
-          <h2>Title: {article.title}</h2>
-          <h3>Autor: {article.author}</h3>
-          <h3> Created at:{article.created_at} </h3>
-          </li>
-        )
+          <div className="article_card">
+            <li key={article.article_id}>
+              <img src={article.article_img_url} className="article_img"></img>
+              <h2 className="article_headings">Title: {article.title}</h2>
+              <h3 className="article_headings">Autor: {article.author}</h3>
+              <h3 className="article_headings"> Votes: {article.votes} </h3>
+              <h3 className="article_headings">
+                {" "}
+                Comments: {article.comment_count}{" "}
+              </h3>
+              <h3 className="article_headings">
+                {" "}
+                Created at: {article.created_at}{" "}
+              </h3>
+              <a className="article_button" href="/article">
+                Read More
+              </a>
+            </li>
+          </div>
+        );
       })}
     </ul>
   );
