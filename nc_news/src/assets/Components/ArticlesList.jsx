b169b4bcad80303638922b8c7e8e819ad3732268
Rayhan Elbeera <raybeera2014@gmail.com>
2024-03-12 09:34:34 +0000

    removed console.log

--- nc_news/src/assets/Components/ArticlesList.jsx
+++ nc_news/src/assets/Components/ArticlesList.jsx
@@ -14,7 +14,6 @@ const ArticlesList = () => {
   return (
     <ul className="articles-list">
       {allArticles.map((article) => {
-        console.log(article);
         return (
           <div className="article_card">
             <li key={article.article_id}>
